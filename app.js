c1ea9dfd1b2ad53c40b7b3e96e66bfdfee012b91
AndrewL <lawna12@gmail.com>
2020-02-20 14:02:45 -0500

    connection working PORT 8080

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-var password = require("./password");
-var inquirer = require("inquirer");
-var mysql = require("mysql");
+const password = require("./password");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require("console.table");
+
+require('events').EventEmitter.defaultMaxListeners = 15;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: process.env.PORT || 8080,
+    user: "root",
+    password: password,
+    database: "Employee_DB"
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start();
+})
+
+console.log("working")
